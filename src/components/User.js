cad7d55ce784beb1814c60d08f222795dba377ee
pinkskie <lipn5757@gmail.com>
2022-01-23 17:25:41 +0600

    userId link

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const User = () => {
   const {id} = useParams();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const getUser = async () => {
+      const user = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      const res = await user.json()
+      setUser(res)
+    }
+    getUser()
+  }, [id])
+
   return (
     <div>
-      <h1>{id}</h1>
+      <h1>{user.username}</h1>
     </div>
   )
 }
